05afd258446a93d40f55b006602fa3011181a4f5
Patschka Sascha <patschka.sascha@live.com>
2019-08-20 12:00:32 +0200

    Initial Commit with all projects and ViewModel-Infrastructure

---
+++ LRPC_CommunityProject/LRPC.ViewModel/Infrastructure/ViewModelBase.vb
@@ -0,0 +1,44 @@
+﻿
+Imports System.ComponentModel
+Imports System.Runtime.CompilerServices
+
+Namespace Infrastructure
+    Public MustInherit Class ViewModelBase
+        Implements INotifyPropertyChanged
+
+        Private Shared ReadOnly HostProcesses As New List(Of String)({"XDesProc", "devenv", "WDExpress"})
+
+        ''' <summary>
+        ''' Gibt zurück ob sich die ausführung des Codes aktuell in der Entwicklungszeit oder in der Laufzeit befindet.
+        ''' </summary>
+        ''' <returns>Wird Code des ViewModels vom XAML Designer ausgeführt wird True zurückgegeben.</returns>
+        Public ReadOnly Property IsInDesignMode As Boolean
+            Get
+                Dim ret = HostProcesses.Contains(Process.GetCurrentProcess().ProcessName)
+                Return ret
+            End Get
+        End Property
+
+
+
+
+
+#Region "INotifyPropertyChanged"
+        Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
+
+        ''' <summary>
+        ''' Prozedur wirft den INotifyPropertyChanged Event welcher in der WPF benötigt wird um die UI zu verständingen
+        ''' das eine Änderung an einem Property stattgefunden hat.
+        ''' </summary>
+        ''' <param name="prop">Das Propertie welches sich geändert hat. Ist Optional da als Parameter "CallerMemberName" verwendet wird. Wird Nothing übergeben werden alle PRoperties des Views aktualisiert!!</param>
+        Protected Overridable Sub RaisePropertyChanged(<CallerMemberName> Optional ByVal prop As String = "")
+            RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(prop))
+        End Sub
+
+
+#End Region
+
+
+    End Class
+End Namespace
