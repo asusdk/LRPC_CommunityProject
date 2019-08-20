c477110bd00062fd593db10d1d8709ebfc2f54f1
marco kenngott <asusexcite@gmail.com>
2019-08-20 21:36:04 +0200

    Modelclasses edited

--- LRPC_CommunityProject/LRPC.Model/Product.vb
+++ LRPC_CommunityProject/LRPC.Model/Product.vb
@@ -3,5 +3,19 @@
     Public Property Id As Guid = Guid.NewGuid()
 
 
+    Public Property ProductName As String = "unnamed Product"
 
+    Public Property ProductPrice As Decimal = 0
+
+    Public Property MaintenanceKitPrice As Decimal = 0
+
+    Public Property RentsUntilMaintenance As Double = 0
+
+    Public Property HoursOfCheck As Double = 0
+
+    Public Property ConsumablesPrice As Decimal = 0
+
+    Public Property ShippingFee As Decimal = 0
+
+    Public Property ProductComment As String = String.Empty
 End Class
