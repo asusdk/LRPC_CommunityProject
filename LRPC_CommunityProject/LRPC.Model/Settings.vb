f61af5f888634509af297030c96c6b08c2c7fb5a
marco kenngott <asusexcite@gmail.com>
2019-08-20 20:56:55 +0200

    Product-Klasse erstellt Settings-Klasse erstellt

---
+++ LRPC_CommunityProject/LRPC.Model/Settings.vb
@@ -0,0 +1,12 @@
+﻿<Serializable>
+Public Class Settings
+
+
+    Public Property Labor As Decimal = 115
+
+    Public Property PriceSplitter As Decimal = 40
+
+    Public Property XmlPath As String = AppDomain.CurrentDomain.BaseDirectory & "\LRPC_DB.xml"
+
+
+End Class
