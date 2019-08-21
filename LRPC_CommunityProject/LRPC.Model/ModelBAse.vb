76b6abec054757175854be4c3d36316d5c41affa
Patschka Sascha <patschka.sascha@live.com>
2019-08-21 08:22:31 +0200

    Modelvalidierung anhand der DataAnnotations implementiert.

--- LRPC_CommunityProject/LRPC.Model/ModelBAse.vb
+++ LRPC_CommunityProject/LRPC.Model/ModelBAse.vb
@@ -1,4 +1,6 @@
-﻿<Serializable>
+﻿Imports System.ComponentModel.DataAnnotations
+
+<Serializable>
 Public MustInherit Class ModelBase
 
     Public Property Id As Guid = Guid.NewGuid()
@@ -10,5 +12,22 @@ Public MustInherit Class ModelBase
     Public Property LastModifiedBy As String = Environment.UserName
 
     Public Property LastModifiedTimeStamp As DateTime = DateTime.Now
+
+
+    ''' <summary>
+    ''' Gibt vorhandene Validierungsfehler der Modelinstanz zurück.
+    ''' </summary>
+    ''' <returns>Eine Auflistung von ValidationResults</returns>
+    Public Overridable Function Validate() As IEnumerable(Of ValidationResult)
+        Return ModelValidator.ValidateEntity(Me)
+    End Function
+
+    ''' <summary>
+    ''' Gibt zurück ob das Model Validiert werden konnte oder nicht, also ob alle Eigenschaften korrekte Werte besitzen.
+    ''' </summary>
+    ''' <returns>Gibt True zurück wenn es keine Validierungsfehler gibt</returns>
+    Public Function IsValid() As Boolean
+        Return Validate() Is Nothing OrElse Validate.Count = 0
+    End Function
 
 End Class
