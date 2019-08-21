16ffb8175f751f876da6c930fde30acf99443d4a
Patschka Sascha <patschka.sascha@live.com>
2019-08-21 12:51:34 +0200

    Modelvalidator vereinfacht

--- LRPC_CommunityProject/LRPC.App/Application.xaml.vb
+++ LRPC_CommunityProject/LRPC.App/Application.xaml.vb
@@ -1,4 +1,9 @@
-﻿Class Application
+﻿Imports LRPC.Model
+
+Class Application
+    Private Sub Application_Startup(sender As Object, e As StartupEventArgs) Handles Me.Startup
+
+    End Sub
 
     ' Ereignisse auf Anwendungsebene wie Startup, Exit und DispatcherUnhandledException
     ' können in dieser Datei verarbeitet werden.
--- LRPC_CommunityProject/LRPC.Model/ModelBAse.vb
+++ LRPC_CommunityProject/LRPC.Model/ModelBAse.vb
@@ -14,20 +14,14 @@ Public MustInherit Class ModelBase
     Public Property LastModifiedTimeStamp As DateTime = DateTime.Now
 
 
-    ''' <summary>
-    ''' Gibt vorhandene Validierungsfehler der Modelinstanz zurück.
-    ''' </summary>
-    ''' <returns>Eine Auflistung von ValidationResults</returns>
-    Public Overridable Function Validate() As IEnumerable(Of ValidationResult)
-        Return ModelValidator.ValidateEntity(Me)
+    Public Function Validate() As IList(Of ValidationResult)
+        Dim validationResults As New List(Of ValidationResult)()
+        Dim validationContext As New ValidationContext(Me)
+        Validator.TryValidateObject(Me, validationContext, validationResults, True)
+        Return validationResults
     End Function
-
-    ''' <summary>
-    ''' Gibt zurück ob das Model Validiert werden konnte oder nicht, also ob alle Eigenschaften korrekte Werte besitzen.
-    ''' </summary>
-    ''' <returns>Gibt True zurück wenn es keine Validierungsfehler gibt</returns>
     Public Function IsValid() As Boolean
-        Return Validate() Is Nothing OrElse Validate.Count = 0
+        Return Validate().Count = 0
     End Function
 
 End Class
