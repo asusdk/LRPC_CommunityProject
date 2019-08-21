317722b5729ce44479de6d9d093877a37c00735a
marco kenngott <asusexcite@gmail.com>
2019-08-21 07:01:56 +0200

    Änderung an vorhandenen Klassen, DataAnnotations hinzugefügt

---
+++ LRPC_CommunityProject/LRPC.Model/ModelBAse.vb
@@ -0,0 +1,14 @@
+﻿<Serializable>
+Public MustInherit Class ModelBase
+
+    Public Property Id As Guid = Guid.NewGuid()
+
+    Public Property CreatedBy As String = Environment.UserName
+
+    Public Property CreationTimeStamp As DateTime = DateTime.Now
+
+    Public Property LastModifiedBy As String = Environment.UserName
+
+    Public Property LastModifiedTimeStamp As DateTime = DateTime.Now
+
+End Class
--- LRPC_CommunityProject/LRPC.Model/Settings.vb
+++ LRPC_CommunityProject/LRPC.Model/Settings.vb
@@ -1,12 +1,20 @@
-﻿<Serializable>
+﻿Imports System.ComponentModel.DataAnnotations
+
+<Serializable>
 Public Class Settings
+    Inherits ModelBase
 
-
+    <Range(1, 999, ErrorMessage:="Labor accepts values between {1} and {2}")>
     Public Property Labor As Decimal = 115
 
+    <Range(1, 100, ErrorMessage:="PriceSplitter accepts values between {1} and {2}")>
     Public Property PriceSplitter As Decimal = 40
 
+    <Required(ErrorMessage:="Path has to be entered")>
     Public Property XmlPath As String = AppDomain.CurrentDomain.BaseDirectory & "\LRPC_DB.xml"
 
 
+    Public Overrides Function ToString() As String
+        Return $"{Labor} - {PriceSplitter} - {XmlPath}"
+    End Function
 End Class
